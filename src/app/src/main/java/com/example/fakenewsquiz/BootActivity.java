1c67fe841c32ce325c263c035c0d3bc6ba84a2e7
Cythraul <dominik.hilber@gmail.com>
2020-04-05 12:09:19 +0200

    added Bootscreen

---
+++ src/app/src/main/java/com/example/fakenewsquiz/BootActivity.java
@@ -0,0 +1,26 @@
+package com.example.fakenewsquiz;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class BootActivity extends AppCompatActivity {
+
+    public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_boot);
+    }
+
+    /** Called when the user taps the Send button */
+    public void start(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        String message = "start";
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
+}
